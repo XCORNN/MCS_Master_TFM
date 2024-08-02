958c403eb293572dfa1fa770edf8d83dfa649061
XCORNN <72931742+XCORNN@users.noreply.github.com>
2024-08-02 10:53:48 +0200

    Update V2_install_All.sh

--- V2_install_All.sh
+++ V2_install_All.sh
@@ -27,28 +27,48 @@ check_success() {
     fi
 }
 
-# Clone the repository (if not already cloned)
-if [ ! -d "MCS_Master_TFM" ]; then
+# Function to clone the repository
+clone_repo() {
     echo "Cloning the repository..."
-    git clone "$REPO_URL.git"
+    git clone "$REPO_URL"
     check_success "git clone"
+}
+
+# Check if inside MCS_Master_TFM and if Instalaciones_desatendidas exists
+if [ -d "Instalaciones_desatendidas" ]; then
+    echo "Already inside the repository directory."
+else
+    # Navigate up to see if we're in a subdirectory
+    cd .. || { echo "Failed to navigate up a directory"; exit 1; }
+    
+    if [ -d "MCS_Master_TFM" ]; then
+        echo "Navigating to the repository directory."
+        cd MCS_Master_TFM || { echo "Failed to change to MCS_Master_TFM directory"; exit 1; }
+    else
+        # If not in a subdirectory of the repo, clone it
+        clone_repo
+        cd MCS_Master_TFM || { echo "Failed to change to MCS_Master_TFM directory"; exit 1; }
+    fi
+    
+    # Check again if Instalaciones_desatendidas exists
+    if [ ! -d "Instalaciones_desatendidas" ]; then
+        echo "Failed to find Instalaciones_desatendidas after cloning."
+        exit 1
+    fi
 fi
 
 # Navigate to the directory containing the scripts
-cd MCS_Master_TFM/Instalaciones_desatendidas || { echo "Failed to change directory"; exit 1; }
+cd Instalaciones_desatendidas || { echo "Failed to change directory to Instalaciones_desatendidas"; exit 1; }
 
 # Make all scripts executable
 echo "Setting execute permissions for all scripts..."
 chmod +x *.sh
 check_success "chmod +x *.sh"
 
-# Navigate back to the main directory
-cd ../.. || { echo "Failed to return to the main directory"; exit 1; }
-
 # Execute each script in the sequence
 for script in "${SCRIPTS[@]}"; do
     echo "Executing $script..."
-    bash "MCS_Master_TFM/Instalaciones_desatendidas/$script"
+    bash "./$script"
     check_success "$script"
 done
 
