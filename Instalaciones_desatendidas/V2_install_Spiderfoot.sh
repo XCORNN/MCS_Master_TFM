71ac9a772206438ec4a851c283bf1d8668f95c14
XCORNN <72931742+XCORNN@users.noreply.github.com>
2024-08-02 12:32:37 +0200

    Update V2_install_Spiderfoot.sh

--- Instalaciones_desatendidas/V2_install_Spiderfoot.sh
+++ Instalaciones_desatendidas/V2_install_Spiderfoot.sh
@@ -9,31 +9,58 @@ depriv() {
   fi
 }
 
-# Verifica si el script se está ejecutando como root
-if [ "$(id -u)" -ne "0" ]; then
-    echo "Este script debe ejecutarse como root"
-    exit 1
-fi
+# Obtiene el directorio de escritorio del usuario actual
+DEST_DIR="/home/$SUDO_USER/Escritorio/Spiderfoot"
 
-# Instalación de Spiderfoot
-echo "Instalando Spiderfoot..."
-depriv mkdir -p ~/Escritorio/Spiderfoot
-depriv bash -c 'cd ~/Escritorio/Spiderfoot && wget https://github.com/smicallef/spiderfoot/archive/refs/tags/v4.0.tar.gz'
-
-# Instalación de dependencias
-echo "Instalando dependencias..."
-if command -v pip3 &>/dev/null; then
-    depriv pip3 install lxml netaddr cherrypy mako requests bs4 pyyaml --break-system-packages
+# Crea el directorio de destino si no existe
+depriv bash -c "
+if [ ! -d '$DEST_DIR' ]; then
+    echo 'Creando el directorio $DEST_DIR...'
+    mkdir -p '$DEST_DIR'
+    if [ $? -ne 0 ]; then
+        echo 'Error al crear el directorio $DEST_DIR. Verifica los permisos.'
+        exit 1
+    fi
 else
-    depriv apt-get install -y python3-pip
-    depriv pip3 install lxml netaddr cherrypy mako requests bs4 pyyaml --break-system-packages
+    echo 'El directorio $DEST_DIR ya existe.'
 fi
+"
 
-# Instalación de m2crypto
-depriv apt-get install -y python3-m2crypto
+# Descarga el archivo tar.gz de Spiderfoot
+cd "$DEST_DIR"
+wget https://github.com/smicallef/spiderfoot/archive/refs/tags/v4.0.tar.gz
 
-# Extracción e instalación de Spiderfoot
-echo "Extrayendo e instalando Spiderfoot..."
-depriv bash -c 'cd ~/Escritorio/Spiderfoot && tar xzvf v4.0.tar.gz && cd spiderfoot-4.0 && sed -i "/pyyaml/d" requirements.txt && pip3 install -r requirements.txt --break-system-packages'
+# Crea y activa un entorno virtual para Python
+depriv bash -c "
+cd '$DEST_DIR'
+python3 -m venv venv
+source venv/bin/activate
+"
 
-echo "Script completado."
+# Instala las dependencias necesarias dentro del entorno virtual
+depriv bash -c "
+cd '$DEST_DIR'
+source venv/bin/activate
+pip install lxml netaddr cherrypy mako requests bs4 pyyaml --break-system-packages
+"
+
+# Instala python3-m2crypto usando apt
+sudo apt-get install -y python3-m2crypto
+
+# Descomprime el archivo tar.gz y mueve el contenido
+depriv bash -c "
+cd '$DEST_DIR'
+tar xzvf v4.0.tar.gz
+mv spiderfoot-4.0/* .
+rm -rf spiderfoot-4.0
+"
+
+# Instala las dependencias de Spiderfoot desde requirements.txt
+depriv bash -c "
+cd '$DEST_DIR'
+source venv/bin/activate
+sed -i '/pyyaml/d' requirements.txt
+pip install -r requirements.txt --break-system-packages
+"
+
+echo "Instalación completa de Spiderfoot"
