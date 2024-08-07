002dce2ed045ac89a7e33712599d7f11eead834d
XCORNN <72931742+XCORNN@users.noreply.github.com>
2024-08-07 12:41:58 +0200

    Update V3_Personalizacion.sh

--- Personalizacion/V3_Personalizacion.sh
+++ Personalizacion/V3_Personalizacion.sh
@@ -85,5 +85,5 @@ chmod 644 /usr/share/desktop-directories/information-gathering-tools.directory'
 
 # 9. Cierre de sesión
 echo "Reiniciando GNOME Shell para aplicar los cambios..."
-depriv gnome-session-quit --logout --no-prompt
+gnome-session-quit --logout --no-prompt
 
