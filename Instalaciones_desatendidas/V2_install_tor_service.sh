5961497fba8b877a07983dd06735a30f6ad594ac
XCORNN <72931742+XCORNN@users.noreply.github.com>
2024-08-08 11:47:09 +0200

    Update V2_install_tor_service.sh

--- Instalaciones_desatendidas/V2_install_tor_service.sh
+++ Instalaciones_desatendidas/V2_install_tor_service.sh
@@ -10,12 +10,15 @@ fi
 
 # Actualiza el índice de paquetes e instala apt-transport-https
 echo "Instalando apt-transport-https..."
-apt update
-apt install -y apt-transport-https
+apt-get update -y
+apt-get install -y apt-transport-https
 
 # Agrega la clave del repositorio de Tor
 echo "Agregando la clave del repositorio de Tor..."
-wget -qO- https://deb.torproject.org/torproject.org/A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89.asc | gpg --dearmor -o /usr/share/keyrings/deb.torproject.org-keyring.gpg
+if ! wget -qO- https://deb.torproject.org/torproject.org/A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89.asc | gpg --dearmor -o /usr/share/keyrings/deb.torproject.org-keyring.gpg; then
+    echo "Error al agregar la clave del repositorio de Tor"
+    exit 1
+fi
 
 # Obtiene el nombre del código de la distribución
 DIST=$(lsb_release -c | awk '{print $2}')
@@ -27,13 +30,23 @@ echo "deb-src [signed-by=/usr/share/keyrings/deb.torproject.org-keyring.gpg] htt
 
 # Actualiza el índice de paquetes e instala Tor y el keyring de Tor
 echo "Actualizando el índice de paquetes e instalando Tor..."
-apt update
-apt install -y tor deb.torproject.org-keyring
+apt-get update -y
+if ! apt-get install -y tor deb.torproject.org-keyring; then
+    echo "Error al instalar Tor"
+    exit 1
+fi
 
 # Inicia y habilita el servicio Tor
 echo "Iniciando y habilitando el servicio Tor..."
-systemctl start tor >/dev/null 2>&1
-systemctl enable tor >/dev/null 2>&1
+if ! systemctl start tor >/dev/null 2>&1; then
+    echo "Error al iniciar el servicio Tor"
+    exit 1
+fi
+
+if ! systemctl enable tor >/dev/null 2>&1; then
+    echo "Error al habilitar el servicio Tor"
+    exit 1
+fi
 
 echo "Tor se ha instalado y está ejecutándose."
 
