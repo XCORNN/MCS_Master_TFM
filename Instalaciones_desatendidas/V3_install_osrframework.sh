888afe4c0f4084dea4334035d30ec56b7a48560a
XCORNN <72931742+XCORNN@users.noreply.github.com>
2024-08-15 13:27:21 +0200

    Update V3_install_osrframework.sh

--- Instalaciones_desatendidas/V3_install_osrframework.sh
+++ Instalaciones_desatendidas/V3_install_osrframework.sh
@@ -19,8 +19,7 @@ fi
 DEST_DIR="/home/$SUDO_USER/Escritorio/osrframework-venv"
 
 # Instalar paquetes necesarios
-depriv sudo apt update
-depriv sudo apt install -y python3 python3-pip python3-venv
+depriv bash -c "sudo apt update && sudo apt install -y python3 python3-pip python3-venv"
 
 # Crea el directorio de destino si no existe
 depriv bash -c "
