7ce5d4f87ef8ed06115d579234a5281014bb861f
XCORNN <72931742+XCORNN@users.noreply.github.com>
2024-08-22 11:42:53 +0200

    Update V2_Foca_sqlExpress.sh

--- Instalaciones_desatendidas/V2_Foca_sqlExpress.sh
+++ Instalaciones_desatendidas/V2_Foca_sqlExpress.sh
@@ -1,90 +1,90 @@
 #!/bin/bash -e
 
-# Use the following variables to control your install:
+# Usa las siguientes variables para controlar la instalación:
 
-# Password for the SA user (required)
+# Contraseña para el usuario SA (requerido)
 MSSQL_SA_PASSWORD='Passw0rd!'
 
-# Product ID of the version of SQL Server you're installing
-# Must be evaluation, developer, express, web, standard, enterprise, or your 25 digit product key
-# Defaults to developer
+# ID de producto de la versión de SQL Server que estás instalando
+# Debe ser evaluation, developer, express, web, standard, enterprise, o tu clave de producto de 25 dígitos
+# Por defecto es developer
 MSSQL_PID='express'
 
-# Enable SQL Server Agent (recommended)
+# Habilitar SQL Server Agent (recomendado)
 SQL_ENABLE_AGENT='y'
 
-# Install SQL Server Full Text Search (optional)
+# Instalar SQL Server Full Text Search (opcional)
 # SQL_INSTALL_FULLTEXT='y'
 
-# Create an additional user with sysadmin privileges (optional)
+# Crear un usuario adicional con privilegios de sysadmin (opcional)
 # SQL_INSTALL_USER='<Username>'
 # SQL_INSTALL_USER_PASSWORD='<YourStrong!Passw0rd>'
 
 if [ -z $MSSQL_SA_PASSWORD ]
 then
-  echo Environment variable MSSQL_SA_PASSWORD must be set for unattended install
+  echo La variable de entorno MSSQL_SA_PASSWORD debe estar configurada para la instalación desatendida
   exit 1
 fi
-echo Running apt-get update -y...
+echo Ejecutando apt-get update -y...
 sudo apt-get update -y
 sudo apt-get install -y curl
 
-echo Adding Microsoft repositories...
-# Add Microsoft GPG key
+echo Agregando repositorios de Microsoft...
+# Agregar clave GPG de Microsoft
 curl -s https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
-# Add SQL Server 2022 repository for Ubuntu 22.04
+# Agregar el repositorio de SQL Server 2022 para Ubuntu 22.04
 sudo wget -O /etc/apt/sources.list.d/mssql-server-2022.list https://packages.microsoft.com/config/ubuntu/22.04/mssql-server-2022.list
-# Add prod repository
+# Agregar repositorio de producción
 sudo wget -O /etc/apt/sources.list.d/prod.list https://packages.microsoft.com/config/ubuntu/22.04/prod.list
 
-echo Running apt-get update -y...
+echo Ejecutando apt-get update -y...
 sudo apt-get update -y
 
-echo Installing SQL Server...
+echo Instalando SQL Server...
 sudo apt-get install -y mssql-server
 
-echo Running mssql-conf setup...
+echo Ejecutando mssql-conf setup...
 sudo MSSQL_SA_PASSWORD=$MSSQL_SA_PASSWORD \
      MSSQL_PID=$MSSQL_PID \
      /opt/mssql/bin/mssql-conf -n setup accept-eula
 
-echo Installing mssql-tools and unixODBC developer...
+echo Instalando mssql-tools y unixODBC developer...
 sudo ACCEPT_EULA=Y apt-get install -y mssql-tools unixodbc-dev
 
-# Optional Enable SQL Server Agent:
+# Habilitar SQL Server Agent (opcional):
 if [ ! -z $SQL_ENABLE_AGENT ]
 then
-  echo Enabling SQL Server Agent...
+  echo Habilitando SQL Server Agent....
   sudo /opt/mssql/bin/mssql-conf set sqlagent.enabled true
 fi
 
-# Optional SQL Server Full Text Search installation:
+# Instalación opcional de SQL Server Full Text Search:
 if [ ! -z $SQL_INSTALL_FULLTEXT ]
 then
-    echo Installing SQL Server Full-Text Search...
+    echo Instalando SQL Server Full-Text Search...
     sudo apt-get install -y mssql-server-fts
 fi
 
-# Optional example of post-installation configuration.
-# Trace flags 1204 and 1222 are for deadlock tracing.
-# echo Setting trace flags...
+# Ejemplo opcional de configuración posterior a la instalación.
+# Las flags de rastreo 1204 y 1222 son para el rastreo de interbloqueos.
+# echo Configurando flags de rastreo...
 # sudo /opt/mssql/bin/mssql-conf traceflag 1204 1222 on
 
-# Add SQL Server tools to the path by default:
-echo Adding SQL Server tools to your path...
+# Agregar herramientas de SQL Server al PATH por defecto:
+echo Agregando herramientas de SQL Server a tu PATH...
 echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
 source ~/.bashrc
 
-# Restart SQL Server after installing:
-echo Restarting SQL Server...
+# Reiniciar SQL Server después de la instalación:
+echo Reiniciando SQL Server...
 sudo systemctl restart mssql-server
 
-# Connect to server and get the version:
+# Conectar al servidor y obtener la versión:
 counter=1
 errstatus=1
 while [ $counter -le 5 ] && [ $errstatus = 1 ]
 do
-  echo Waiting for SQL Server to start...
+  echo Esperando a que SQL Server se inicie...
   sleep 3s
   /opt/mssql-tools/bin/sqlcmd \
     -S localhost \
@@ -95,14 +95,14 @@ do
   ((counter++))
 done
 
-# Display error if connection failed:
+# Mostrar error si la conexión falló:
 if [ $errstatus = 1 ]
 then
-  echo Cannot connect to SQL Server, installation aborted
+  echo No se puede conectar a SQL Server, instalación abortada
   exit $errstatus
 fi
 
-# Optional new user creation:
+# Creación opcional de un nuevo usuario:
 if [ ! -z $SQL_INSTALL_USER ] && [ ! -z $SQL_INSTALL_USER_PASSWORD ]
 then
   echo Creating user $SQL_INSTALL_USER
@@ -116,7 +116,7 @@ fi
 echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
 source ~/.bashrc
 
-echo Done!
+echo ¡Hecho!
 
 #para comprobar si se ha hecho correctamente:
 # /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P 'Passw0rd!'
